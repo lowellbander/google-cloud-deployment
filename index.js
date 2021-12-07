3d8c3c83847446b3701e396b8d935b557916dde2
Lowell Bander <lowellbander@gmail.com>
2021-12-07 03:05:21 +0000

    hook test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('Here it is, the aid app!');
+  res.send('Here it is, the aid app! With tweaked text!');
 });
 
 const PORT = process.env.PORT || 3000;
